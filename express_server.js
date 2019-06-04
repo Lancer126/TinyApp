fbbb018d2a1430bcbdbf571a5583aee887ccf817
Lancer126 <rthcontact@gmail.com>
2019-06-04 21:01:28 +0000

    Did the Part 1

--- express_server.js
+++ express_server.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+
+}
 
 app.set("view engine", "ejs")
 
@@ -8,6 +14,15 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
